6c54fb280dc09e79c479c043dc7c2a102c96f278
nariana <nariana.pereira@outlook.com>
2023-11-02 14:29:55 -0300

    Create first db entities and config to db connection

---
+++ myfinance-web-netcore/Domain/AccountPlan.cs
@@ -0,0 +1,14 @@
+
+namespace myfinance_web_netcore.Domain
+{
+    public class AccountPlan
+    {
+        public int Id { get; set;}
+        
+        public string Description { get; set;}
+
+        public string Type { get; set;}
+
+
+    }
+}
--- myfinance-web-netcore/Infrastructure/MyFinanceDbContext.cs
+++ myfinance-web-netcore/Infrastructure/MyFinanceDbContext.cs
@@ -1,12 +1,23 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using myfinance_web_netcore.Domain;
 
 namespace myfinance_web_netcore.Infrastructure
 {
-    public class MyFinanceDbContext
+    public class MyFinanceDbContext : DbContext
     {
-        
+
+        public DbSet<AccountPlan> AccountPlan {get; set;}
+
+        public DbSet<AccountTransaction> AccountTransaction {get; set;}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = @"Server=localhost:1433;Database=myfinanceweb;User Id=sa;Password=Nari3714#;"; 
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+
+
     }
 }
